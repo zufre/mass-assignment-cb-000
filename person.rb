deb3897046ada1079d3225020233cbd3576d2d17
Steven Nunez <steven.nunez@gmail.com>
2014-06-02 12:29:37 -0400

    Add Alternate solution. Add methods to instance singleton

--- person.rb
+++ person.rb
@@ -9,13 +9,26 @@ class Person
   private
 
   def create_accessors(method, value)
-    self.class.instance_eval do
-      define_method "#{method}=" do |value|
-        instance_variable_set("@#{method}", value)
+    # Adds code to every instance
+    # self.class.instance_eval do
+    #   define_method "#{method}=" do |value|
+    #     instance_variable_set("@#{method}", value)
+    #   end
+    #   define_method method do
+    #     instance_variable_get("@#{method}")
+    #   end
+    # end
+    #
+    # Adds code to each instance separately. 2 Instances won't have the same
+    # accessors unless their keys are are the same during initialization
+    instance_eval <<-CODE
+      def #{method}=(v)
+        @#{method} = v
       end
-      define_method method do
-        instance_variable_get("@#{method}")
+
+      def #{method}
+        @#{method}
       end
-    end
+    CODE
   end
 end
