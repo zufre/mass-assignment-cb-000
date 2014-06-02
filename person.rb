3b88b5bd56de79b3a4551bd68cb4a0e6372392e9
Steven Nunez <steven.nunez@gmail.com>
2014-06-01 20:06:10 -0400

    Update to use kwargs and dynamic method creation

--- person.rb
+++ person.rb
@@ -1,10 +1,21 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
-  def initialize(attributes=nil)
-    if attributes
-      attributes.each do |k,v|
-        self.send("#{k}=", v)
+  def initialize(**kwargs)
+    kwargs.each_pair do |method, value|
+      create_accessors(method, value)
+    end
+  end
+
+  private
+
+  def create_accessors(method, value)
+    self.class.instance_eval do
+      define_method "#{method}=" do |value|
+        instance_variable_set("@#{method}", value)
+      end
+      define_method method do
+        instance_variable_get("@#{method}")
       end
     end
+    public_send("#{method}=", value)
   end
 end
