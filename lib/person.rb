a53a82a25d8d5b5af0f56c86dd3db7b8cf24e56c
Cedric David <cedric.david@btopenworld.com>
2018-09-07 06:08:25 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,10 @@
 class Person
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+  :weight, :handed, :complexion, :t_shirt_size, 
+  :wrist_size, :glove_size, :pant_length, :pant_width
 
-
+  def initialize(attributes)
+    attributes.each {|key, value|
+    self.send(("#{key}="), value)}  
+  end
 end
