b04d3fd8737879d652530727ab0ff521e52c10b3
Cedric David <cedric.david@btopenworld.com>
2018-09-07 06:08:28 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
-  :weight, :handed, :complexion, :t_shirt_size, 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
   :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
